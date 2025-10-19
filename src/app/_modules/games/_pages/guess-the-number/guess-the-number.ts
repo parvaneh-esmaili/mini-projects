752a91d9b1c34bee39f32277fb4d6471cc159dc2
Parvaneh esmaili <parvanehun@gmail.com>
2025-10-19 14:15:02 -0700

    ADD: Add Guess The Nubmer Component

---
+++ src/app/_modules/games/_pages/guess-the-number/guess-the-number.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guess-the-number',
+  imports: [],
+  templateUrl: './guess-the-number.html',
+  styleUrl: './guess-the-number.css'
+})
+export class GuessTheNumber {
+
+}
