824bc7d2e0a99a0b2f5090bbf3cf1a35b5bd342d
Parvaneh esmaili <parvanehun@gmail.com>
2025-10-25 11:29:10 -0700

    Fix: Fix Memory Game

--- src/app/_modules/games/games-routing-module.ts
+++ src/app/_modules/games/games-routing-module.ts
@@ -4,12 +4,14 @@ import { Home } from './_pages/home/home';
 import { RockPaperScissors } from './_pages/rock-paper-scissors/rock-paper-scissors';
 import { Games } from './games';
 import { GuessTheNumber } from './_pages/guess-the-number/guess-the-number';
+import { MemoryGame } from './_pages/memory-game/memory-game';
 
 const routes: Routes = [
   {path: '', component: Games, children:[
     { path: '', component: Home, pathMatch: 'full' },
     { path: 'rock-paper-scissors', component:  RockPaperScissors },
     { path: 'guess-the-number', component:  GuessTheNumber },
+    { path: 'memory-game', component: MemoryGame}
 
   ]}
 ];
