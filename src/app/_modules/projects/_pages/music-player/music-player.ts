4cfed1aa18d9a0330227cccc347a5dfa46de59c8
Parvaneh esmaili <parvanehun@gmail.com>
2025-10-26 08:55:43 -0700

    ADD: Add functions for music player buttons (play, next, prev, forward10, backward10)

--- src/app/_modules/projects/_pages/music-player/music-player.ts
+++ src/app/_modules/projects/_pages/music-player/music-player.ts
@@ -10,10 +10,33 @@ import { Component } from '@angular/core';
 export class MusicPlayer {
 
   buttons= [
-    {icon:'fa-solid fa-rotate-left', class:' h-10 w-10'},
-    {icon:'fa-solid fa-backward', class:' h-12 w-12'},
-    {icon:'fa-solid fa-play', class:' h-14 w-14'},
-    {icon:'fa-solid fa-forward', class:' h-12 w-12'},
-    {icon:'fa-solid fa-rotate-right', class:' h-10 w-10'}
-  ]
+    {icon:'fa-solid fa-rotate-left', class:' h-10 w-10', action:'backward10'},
+    {icon:'fa-solid fa-backward', class:' h-12 w-12', action:'prev'},
+    {icon:'fa-solid fa-play', class:' h-14 w-14', action:'play'},
+    {icon:'fa-solid fa-forward', class:' h-12 w-12', action:'next'},
+    {icon:'fa-solid fa-rotate-right', class:' h-10 w-10', action:'forward10'}
+  ];
+
+handleActions(action: string){
+  const audio = document.querySelector('audio') as HTMLAudioElement;
+  if (!audio) return;
+
+  switch(action){
+    case 'play':
+      console.log('play music')
+      break;
+    case 'prev':
+      console.log('prev music')
+      break;
+    case 'backward10':
+      console.log('backward10')
+      break;
+    case 'next':
+      console.log('next music')
+      break;
+      case 'forward10':
+        console.log('forward10')
+        break
+  }
+}
 }
