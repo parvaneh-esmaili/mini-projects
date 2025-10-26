86e41d61a932e45aae54ed60eedb8ab326d16f07
Parvaneh esmaili <parvanehun@gmail.com>
2025-10-26 08:30:18 -0700

    Make music player buttons dynamic

--- src/app/_modules/projects/_pages/music-player/music-player.ts
+++ src/app/_modules/projects/_pages/music-player/music-player.ts
@@ -1,11 +1,19 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-music-player',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './music-player.html',
   styleUrl: './music-player.css'
 })
 export class MusicPlayer {
 
+  buttons= [
+    {icon:'fa-solid fa-rotate-left', class:' h-10 w-10'},
+    {icon:'fa-solid fa-backward', class:' h-12 w-12'},
+    {icon:'fa-solid fa-play', class:' h-14 w-14'},
+    {icon:'fa-solid fa-forward', class:' h-12 w-12'},
+    {icon:'fa-solid fa-rotate-right', class:' h-10 w-10'}
+  ]
 }
