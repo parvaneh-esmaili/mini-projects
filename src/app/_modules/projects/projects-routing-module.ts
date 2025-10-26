e53cfacc8937343c60e931c4dfb98a1260b50214
Parvaneh esmaili <parvanehun@gmail.com>
2025-10-26 08:16:25 -0700

    ADD: Add Music Player Copmonent And Add Initial Html Structure

--- src/app/_modules/projects/projects-routing-module.ts
+++ src/app/_modules/projects/projects-routing-module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Home } from './_pages/home/home';
 import { Projects } from './projects';
+import { MusicPlayer } from './_pages/music-player/music-player';
 
 const routes: Routes = [
   {
@@ -9,6 +10,8 @@ const routes: Routes = [
     component: Projects,
     children: [
       { path: '', component: Home, pathMatch: 'full' },
+      {path: 'home', component: Home},
+      {path: 'music-player', component: MusicPlayer}
 
     ],
   },
