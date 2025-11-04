47ffa251de805992f93584d65ae521dd4789f1cf
Parvaneh esmaili <parvanehun@gmail.com>
2025-11-04 10:17:37 -0800

    FEAT: Add Draggable Functionality To Task Items

--- src/app/_modules/projects/_pages/todo-list/todo-list.ts
+++ src/app/_modules/projects/_pages/todo-list/todo-list.ts
@@ -7,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrl: './todo-list.css'
 })
 export class TodoList {
+  isMenuOpen = false;
+  newItems = ['hello'];
+  draggedItem: HTMLElement | null = null;
+
+  dragStartHandler(event: DragEvent) {
+    this.draggedItem = event.target as HTMLElement;
+  }
+
+  dropOverHandler(event: DragEvent) {
+    event.preventDefault();
+  }
+
+  dragHandler(event: DragEvent) {
+    event.preventDefault();
+    const dropZone = event.target as HTMLElement;
+
+    if (this.draggedItem && dropZone) {
+      dropZone.appendChild(this.draggedItem);
+      console.log('Item dropped into:', dropZone);
+      this.draggedItem = null;
+    }
+  }
+
+  toggleMenu(){
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  addNewItem(){
+    this.isMenuOpen = false;
+  }
+
+  closeMenu(){
+    this.isMenuOpen = false;
+
+  }
 
 }
