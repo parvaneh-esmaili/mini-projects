5a506d9fffd731a69f54a7fdb348fa4dde49d943
Parvaneh esmaili <parvanehun@gmail.com>
2025-11-20 11:08:42 -0800

    feat: Add Logic To Generate Password Component

--- src/app/_modules/projects/_components/password-generators/password-generators.ts
+++ src/app/_modules/projects/_components/password-generators/password-generators.ts
@@ -5,10 +5,30 @@ import { FormsModule } from '@angular/forms';
   selector: 'app-password-generators',
   imports: [FormsModule],
   templateUrl: './password-generators.html',
-  styleUrl: './password-generators.css'
+  styleUrl: './password-generators.css',
 })
 export class PasswordGenerators {
-  password: string = 'hhluihluih';
-  passwordLength: number= this.password.length
+  password: string = '';
+  passwordLength: number = 6;
 
+  specialCharacters: string = '!@#$%&*';
+  numbers: string = '0987654321';
+  upperCase: string = 'ABCDEFGHIGKLMNOPQRSTUVWXYZ';
+  lowerCase: string = 'abcdefghijklmnopqrstuvwxyz';
+
+  makePassword() {
+    this.password = '';
+
+    const allChars =
+      this.specialCharacters + this.numbers + this.upperCase + this.lowerCase;
+
+    for (let i = 0; i < this.passwordLength; i++) {
+      const passwordGenerator = Math.floor(Math.random() * allChars.length);
+
+      this.password += allChars.charAt(passwordGenerator);
+    }
+    console.log(this.password);
+  }
+
+  copyPassword() {}
 }
