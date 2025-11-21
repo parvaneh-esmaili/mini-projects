92b3859b6b46a3082be03ca271b1fd9fe1174d05
Parvaneh esmaili <parvanehun@gmail.com>
2025-11-21 07:10:05 -0800

    feat: add password strength check and copy functionality

--- src/app/_modules/projects/_components/password-generators/password-generators.ts
+++ src/app/_modules/projects/_components/password-generators/password-generators.ts
@@ -1,9 +1,10 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-password-generators',
-  imports: [FormsModule],
+  imports: [FormsModule, NgClass],
   templateUrl: './password-generators.html',
   styleUrl: './password-generators.css',
 })
@@ -12,8 +13,8 @@ export class PasswordGenerators {
   passwordLength: number = 6;
 
   specialCharacters: string = '!@#$%&*';
-  numbers: string = '0987654321';
-  upperCase: string = 'ABCDEFGHIGKLMNOPQRSTUVWXYZ';
+  numbers: string = '0123456789';
+  upperCase: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   lowerCase: string = 'abcdefghijklmnopqrstuvwxyz';
 
   exclude = {
@@ -23,21 +24,32 @@ export class PasswordGenerators {
     lower: true,
   };
 
+  securityLevel: number = 0;
+
+  evaluateSecurity(password: string) {
+    let score = 0;
+
+    if (password.length >= 12) score += 2;
+    else if (password.length >= 8) score += 1;
+
+    if (/[a-z]/.test(password)) score += 1;
+    if (/[A-Z]/.test(password)) score += 1;
+    if (/[0-9]/.test(password)) score += 1;
+    if (/[^a-zA-Z0-9]/.test(password)) score += 1;
+
+    if (score >= 6) this.securityLevel = 4;
+    else if (score >= 4) this.securityLevel = 3;
+    else if (score >= 2) this.securityLevel = 2;
+    else this.securityLevel = 1;
+  }
+
   makePassword() {
     let allChars = '';
 
-    if (this.exclude.special) {
-      allChars += this.specialCharacters;
-    }
-    if (this.exclude.numbers) {
-      allChars += this.numbers;
-    }
-    if (this.exclude.upper) {
-      allChars += this.upperCase;
-    }
-    if (this.exclude.lower) {
-      allChars += this.lowerCase;
-    }
+    if (this.exclude.special) allChars += this.specialCharacters;
+    if (this.exclude.numbers) allChars += this.numbers;
+    if (this.exclude.upper) allChars += this.upperCase;
+    if (this.exclude.lower) allChars += this.lowerCase;
 
     if (allChars.length === 0) {
       this.password = 'No characters selected!';
@@ -51,8 +63,11 @@ export class PasswordGenerators {
     }
 
     this.password = generatedPassword;
+    this.evaluateSecurity(this.password); 
     console.log(this.password);
   }
 
-  copyPassword() {}
+  copyPassword() {
+    navigator.clipboard.writeText(this.password);
+  }
 }
